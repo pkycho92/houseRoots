af904102d04f28fc26d2e4a09dfab5a5d58b76de
pkycho92 <pkycho92@gmail.com>
2018-11-26 18:51:36 -0800

    commit

---
+++ contactUs.js
@@ -0,0 +1,10 @@
+function initMap() {
+    let houseRoots = { lat: 34.272734, lng: -118.486 };
+    let map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 17,
+            center: houseRoots,
+        }
+    );
+    let marker = new google.maps.Marker({ position: { lat: 34.272117, lng: -118.484339 }, map: map });
+}
